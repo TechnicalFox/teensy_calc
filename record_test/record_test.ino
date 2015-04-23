eb538d79a00a1b649d134b852ba98740bf4b8720
Jim Craveiro <technicalfox@csh.rit.edu>
2015-04-22 23:12:42 -0400

    Working lcd output and remote button read in / interpreting.

---
+++ record_test/record_test.ino
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <IRremote.h>
+#include <openGLCD.h>
+
+int IRpin = 4;
+
+IRrecv irrecv(IRpin);
+decode_results results;
+
+void setup()
+{
+  GLCD.Init();
+  GLCD.SelectFont(System5x7);
+  
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() 
+{
+  if (irrecv.decode(&results)) 
+  {
+    //results.decode_type:
+    //NEC     =  1
+    //SONY    =  2
+    //RC5     =  3
+    //RC6     =  4
+    //UNKNOWN = -1
+    GLCD.printf("Hex value: %x\nType: %i\n\n", results.value, results.decode_type);
+    
+    int i = 0;
+    while ( i < results.rawlen ) {
+      Serial.print(results.rawbuf[i]);
+      i++;
+    }
+    Serial.print("\n");
+    irrecv.resume();   // Receive the next value
+  }
+}
+
---
+++ testing_lcd/testing_lcd.ino
@@ -0,0 +1,32 @@
+#include <openGLCD.h>
+
+void setup()
+{
+  
+  pinMode(4, INPUT);
+  pinMode(13, OUTPUT);
+  
+  GLCD.Init();
+  GLCD.SelectFont(System5x7);
+  
+  GLCD.print("Pin 4 State: ");  
+}
+
+void loop()
+{
+  GLCD.CursorTo(13, 0);
+  int temp = digitalRead(4);
+  GLCD.print(temp);
+  
+  if ( temp == 0 ) {
+    digitalWrite(13, HIGH);
+    delay(100);
+  } else {
+    digitalWrite(13, LOW);
+  }
+  
+  GLCD.CursorTo(0, 2);
+  GLCD.print(millis());
+  
+  
+}
